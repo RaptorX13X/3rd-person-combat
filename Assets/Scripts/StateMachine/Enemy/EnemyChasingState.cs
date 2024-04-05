73ef461b1f108bddeed77aa910829397bafc2f23
Raptor <panraptor13@gmail.com>
2024-04-05 23:16:40 +0200

    Enemy AI halfway

---
+++ Assets/Scripts/StateMachine/Enemy/EnemyChasingState.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChasingState : EnemyBaseState
+{
+    public EnemyChasingState(EnemyStateMachine stateMachine) : base(stateMachine) { }
+
+    private readonly int LocomotionBlendTreeHash = Animator.StringToHash("Locomotion");
+    private readonly int SpeedHash = Animator.StringToHash("Speed");
+    private const float CrossFadeDuration = 0.1f;
+    private const float AnimatorDampTime = 0.1f;
+    public override void Enter()
+    {
+        stateMachine.Animator.CrossFadeInFixedTime(LocomotionBlendTreeHash, CrossFadeDuration);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        if (!IsInChaseRange())
+        {
+            stateMachine.SwitchState(new EnemyIdleState(stateMachine));
+            return;
+        }
+        else if (IsInAttackRange())
+        {
+            stateMachine.SwitchState(new EnemyAttackingState(stateMachine));
+            return;
+        }
+
+        MoveToPlayer(deltaTime);
+        FacePlayer();
+        stateMachine.Animator.SetFloat(SpeedHash, 1f, AnimatorDampTime, deltaTime);
+    }
+
+    public override void Exit()
+    {
+        stateMachine.Agent.ResetPath();
+        stateMachine.Agent.velocity = Vector3.zero;
+    }
+
+    private void MoveToPlayer(float deltaTime)
+    {
+        stateMachine.Agent.destination = stateMachine.Player.transform.position;
+        Move(stateMachine.Agent.desiredVelocity.normalized * stateMachine.MovementSpeed, deltaTime);
+        stateMachine.Agent.velocity = stateMachine.CharacterController.velocity;
+    }
+
+    private bool IsInAttackRange()
+    {
+        float playerDistanceSqr = (stateMachine.Player.transform.position - stateMachine.transform.position).sqrMagnitude;
+        return playerDistanceSqr <= stateMachine.AttackRange * stateMachine.AttackRange;
+    }
+}
